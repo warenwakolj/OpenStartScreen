5c0612e797af7d787b21f824a5205d00cbdd7984
AM_Erizur <50849821+Erizur@users.noreply.github.com>
2024-07-22 10:45:54 -0500

    Update MainWindow.xaml.cs

--- OpenStartScreen/MainWindow.xaml.cs
+++ OpenStartScreen/MainWindow.xaml.cs
@@ -422,7 +422,7 @@ namespace OpenStartScreen
             };
 
             grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(40) });
-            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(150) });
 
             var iconGrid = new Grid
             {
@@ -452,7 +452,7 @@ namespace OpenStartScreen
                 Foreground = Brushes.White,
                 FontSize = 14,
                 TextWrapping = TextWrapping.Wrap,
-                VerticalAlignment = VerticalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Top,
                 HorizontalAlignment = HorizontalAlignment.Left,
                 Margin = new Thickness(10, 0, 0, 0)
             };
